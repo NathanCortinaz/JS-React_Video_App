4c2c8a70aae78c7cd8dad84ca2460112bd397cdd
Nathan Willi de Souza Cortinaz <nathan.cortinaz@htmicron.com.br>
2022-02-21 06:37:50 -0300

    Created main page and components

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Typography, AppBar } from '@material-ui/core';
+
+import VideoPlayer from './components/VideoPlayer';
+import Notifications from './components/Notifications';
+import Options from './components/Options';
+
+import useStyles from './useStyles';
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
+};
+export default App;
